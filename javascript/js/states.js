4f5149fc6285884b70cc8a302646e643b0b024f2
Dave Stearns <dlsinfo@uw.edu>
2015-10-10 14:21:31 -0700

    file stubs for javascript and the dom class

--- javascript/js/states.js
+++ javascript/js/states.js
@@ -1,30 +1,3 @@
 /**
  * application script for the states.html
  */
-document.addEventListener('DOMContentLoaded', function () {
-    'use strict';
-
-    function addStates(states, listElem) {
-        listElem.innerHTML = '';
-
-        states.forEach(function(state) {
-            var li = document.createElement('li');
-            li.textContent = state.name + ' (' + state.abbreviation + ')';
-            listElem.appendChild(li);
-        });
-    }
-
-    var statesListElem = document.getElementById('states-list');
-    addStates(usaStates, statesListElem);
-
-    var statesFilter = document.getElementById('state-filter-field');
-    statesFilter.addEventListener('keyup', function() {
-        var filter = this.value.toLowerCase();
-        var filteredStates = usaStates.filter(function(state) {
-            return filter.length === 0 || state.name.toLowerCase().indexOf(filter) >= 0;
-        });
-
-        addStates(filteredStates, statesListElem);
-    });
-
-});
