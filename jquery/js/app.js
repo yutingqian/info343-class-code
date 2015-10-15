f63e84727ac2675344c8fe56cf162ed0a98c44ca
Dave Stearns <dlsinfo@uw.edu>
2015-10-15 08:42:28 -0700

    add jquery example

---
+++ jquery/js/app.js
@@ -0,0 +1,8 @@
+/*
+    script for the index.html page
+    dependencies: jquery
+
+    open weather API: 
+    http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
+*/
+
