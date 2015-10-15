269d28d7fa8684d6463bbc22713a677c3ec31716
Dave Stearns <dlsinfo@uw.edu>
2015-10-15 08:51:53 -0700

    add leaflet example; use HTTPS for jquery

---
+++ leaflet/js/app.js
@@ -0,0 +1,10 @@
+/*
+    script file for index.html
+    dependencies: leaflet, jquery
+
+    Open Street Maps tile layer URL template:
+    http://{s}.tile.osm.org/{z}/{x}/{y}.png
+
+    attribution:
+    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+*/
