99c4f975b8e890bda831d687f09b5f56aa170b00
Dave Stearns <dlsinfo@uw.edu>
2015-10-21 10:09:44 -0700

    task list code from tuesday

--- parse/js/app.js
+++ parse/js/app.js
@@ -1,3 +1,102 @@
 /*
     script for the index.html file
 */
+
+
+//OK to call this before the DOM is ready
+Parse.initialize("u8fq2u4IqxKXBa9PuPjHB40HA39gqnxMq8lKJYkG", "R9zpakOjl4dXU3quSQ9tvTwwe0uQA2IJj3GdNKTt");
+
+//when the document is ready...
+$(function() {
+    'use strict';
+
+    //define a new Task object class with Parse
+    var Task = Parse.Object.extend('Task');
+
+    //define a query for querying Task objects
+    var tasksQuery = new Parse.Query(Task);
+    tasksQuery.ascending('createdAt');
+
+    //varible to hold the current list of tasks
+    var tasks = [];
+
+    //reference to our error message alert
+    var errorMessage = $('#error-message');
+
+    //reference to the tasks list element
+    var tasksList = $('#tasks-list');
+
+    function displayError(err) {
+        errorMessage.text(err.message);
+        errorMessage.fadeIn();
+    }
+
+    function clearError() {
+        errorMessage.hide();
+    }
+
+    function showSpinner() {
+        $('.fa-spin').show();
+    }
+
+    function hideSpinner() {
+        $('.fa-spin').hide();
+    }
+
+    function onData(results) {
+        tasks = results;
+        renderTasks();
+    }
+
+    function renderTasks() {
+        tasksList.empty();
+        tasks.forEach(function(task) {
+            $(document.createElement('li'))
+                .text(task.get('title'))
+                .appendTo(tasksList);
+        });
+    }
+
+    function fetchTasks() {
+        showSpinner();
+        tasksQuery.find()
+            .then(onData, displayError)
+            .always(hideSpinner);
+    }
+
+    $('#new-task-form').submit(function(evt) {
+        //tell the browser not to do its default behavior
+        evt.preventDefault();
+
+        //find the input element in this form 
+        //with a name attribute set to "title"
+        var titleInput = $(this).find('[name="title"]');
+        
+        //get the current value
+        var title = titleInput.val();
+
+        //create a new Task and set the title
+        var task = new Task();
+        task.set('title', title);
+
+        //save the new task to your Parse database
+        //if save is successful, fetch the tasks again
+        //otherwise display the error
+        task.save()
+            .then(fetchTasks, displayError)
+            .then(function() {
+                titleInput.val('');
+            });
+
+        //some browsers also require that we return false to
+        //prevent the default behavior
+        return false;
+    }); //on new task form submit
+
+    //fetch the tasks to kick everything off...
+    fetchTasks();
+
+    //refetch the tasks every so often
+    //to get new tasks created by others
+    window.setInterval(fetchTasks, 10000);
+}); //on doc ready
