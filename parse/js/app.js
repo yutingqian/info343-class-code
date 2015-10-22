fc4c876e3bc59a5f46dc64840ee7a19be8383703
Yuting Qian <qianyu@uw.edu>
2015-10-22 16:52:19 -0700

    t

--- parse/js/app.js
+++ parse/js/app.js
@@ -13,9 +13,13 @@ $(function() {
     //new query that will return all tasks ordered by createdAt
     var tasksQuery = new Parse.Query(Task);
     tasksQuery.ascending('createdAt');
+    tasksQuery.notEqualTo('done', true);
 
     //reference to the task list element
     var taskList = $('#tasks-list');
+
+    //reference to rating element
+    var ratingElem  = $('#rating');
 
     //reference to the error message alert
     var errorMessage = $('#error-message');
@@ -55,11 +59,29 @@ $(function() {
     function renderTasks() {
         taskList.empty();
         tasks.forEach(function (task) {
-            $(document.createElement('li'))
+            var li = $(document.createElement('li'))
                 .text(task.get('title'))
-                .appendTo(taskList);
+                .addClass(task.get('done') ? 'completed-task' : '')
+                .appendTo(taskList)
+                .click(function() {
+                    task.set('done', !task.get('done'));
+                    task.save().then(renderTasks, displayError);
+            });
+
+            $(document.createElement('span'))
+                .raty({readOnly: true,
+                    score: (task.get('rating') || 0),
+                    hints: ['crap', 'awful', 'okay', 'nice', 'awesome']})
+                .appendTo(li);
         })
     }
+
+  /*  function showMessage(message) {
+        message = message || 'hello';
+        alert(message);
+    }
+
+    showMessage('hello world'); */
 
     //when the user submits the new task form...
     $('#new-task-form').submit(function(evt) {
@@ -69,12 +91,18 @@ $(function() {
         var title = titleInput.val();
         var task = new Task();
         task.set('title', title);
+        task.set('rating', ratingElem.raty('score'));
+
         task.save().then(fetchTasks, displayError).then(function() {
             titleInput.val('');
+            ratingElem.raty('set', {});
         });
 
         return false;
     });
+
+    //enable the rating user interface element
+    ratingElem.raty();
 
     //go and fetch tasks from Parse
     fetchTasks();
