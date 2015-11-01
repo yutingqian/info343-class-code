ba74cccfa73de2e9b672e28781f9f283a67f162d
Dave Stearns <dlsinfo@uw.edu>
2015-10-31 17:49:38 -0700

    add lots and lots of comments

--- angular/js/movies.js
+++ angular/js/movies.js
@@ -2,7 +2,12 @@
     script file for the movies.html file
 */
 
+//define a new Angular module named 'MoviesApp'
+//that depends upon the 'ui.router' module
 angular.module('MoviesApp', ['ui.router'])
+    //define the ratings map as a constant so that it can be injected
+    //into any controller; this map converts a textual rating to a
+    //number that we can use to do proper sorting by rating severity
     .constant('ratingsMap', {
         'Not Rated': 0,
         'G': 1,
@@ -12,15 +17,25 @@ angular.module('MoviesApp', ['ui.router'])
         'NC-17': 5,
         'X': 6
     })
+    //define a factory for getting our movies JSON
+    //a factory is a function that returns something that you want to share
+    //amongst several controllers; we give it a name and that name can be
+    //added to any controller's initialization function
     .factory('moviesJSON', function($http, ratingsMap) {
+        //use Angular's $http service to asynchronously fetch the movies json
         return $http.get('data/movies-2014.min.json')
             .then(function(results) {
-                //results is an object with info about the entire HTTP response
-                //the data itself can be accessed via its `data` property
+                //when that finishes, the `results` parameter here has info about
+                //the entire HTTP response, and the data itself can be accessed via
+                //the `data` property of the `results` object
 
-                //use map to transform the array of movies by adding
+                //use map() to transform the return array of movies by adding
                 //a `ratingOrdinal` property that we can use to sort by
-                //rating severity, and a stable id set to the array index
+                //rating severity, and a stable id
+                //we need the stable id so that we can switch to a detail
+                //view of the movie
+                //map() will return a new array of the same length where
+                //each element is set to whatever our function returns
                 return results.data.map(function(movie, index) {
                     movie.ratingOrdinal = ratingsMap[movie.rating];
                     movie.id = index;
@@ -28,7 +43,15 @@ angular.module('MoviesApp', ['ui.router'])
                 });
             });
     })
+    //add a module configuration function to setup the UI routing
+    //we tell Angular to inject two services from the ui.router module
     .config(function($stateProvider, $urlRouterProvider) {
+        //$stateProvider allows us to define various states, each of
+        //which represents a different view; we specify a local URL
+        //for each state that appears in the browser's address bar,
+        //a url for the Angular view that should be put into the
+        //empty div in movies.html, and the Angular controller to
+        //invoke for that view
         $stateProvider
             .state('list', {
                 url: '/movies',
@@ -36,33 +59,52 @@ angular.module('MoviesApp', ['ui.router'])
                 controller: 'MoviesController'
             })
             .state('detail', {
-                url: '/movies/:movieId',
+                url: '/movies/:movieId', //movie.id will be put on the end of the URL
                 templateUrl: 'views/movies-detail.html',
                 controller: 'MovieDetailController'
             });
 
+        //if the user changes the local URL in the browser address bar
+        //to something other than what we defined above, just reset it
+        //to #/movies
         $urlRouterProvider.otherwise('/movies');
     })
+    //controller for the movies list
+    //we now let Angular inject the movies JSON from the factory defined above
     .controller('MoviesController', function($scope, moviesJSON) {
+        //moviesJSON is still a promise, so we use .then() to register a function
+        //that should be called once the data is available
         moviesJSON.then(function(data) {
             $scope.movies = data;
-
+            //generate an array of the unique distributors so we can let
+            //users filter by distributor name
             $scope.distributors = _.sortBy(_.uniq(_.pluck($scope.movies, 'distributor')));
         });
 
+        //sets the property to sort by
         $scope.setSortProp = function(propName) {
+            //if we are already sorting by that property
+            //reverse the sort order (ascending vs descending)
             if ($scope.sortProp === propName) {
                 $scope.sortReverse = !$scope.sortReverse;
             }
             else {
+                //set the new sort prop and reset the sort order to ascending
                 $scope.sortProp = propName;
                 $scope.sortReverse = false;
             }
         }; //setSortProp
     })
+    //controller for the movie details view
+    //we again tell Angular to inject the movies JSON, which will be the same
+    //data we already fetched from the server for the list view
     .controller('MovieDetailController', function($scope, $stateParams, moviesJSON) {
+        //even though we already have the data, moviesJSON is still a promise,
+        //so we use .then() to register a function to be called with the results
         moviesJSON.then(function(data) {
+            //the $stateParams service lets us get the movie ID off the local URL
+            //so we can find the particular movie we are showing
             $scope.movie = data[$stateParams.movieId];
         });
 
-    });
+    }); //MoviesApp module
