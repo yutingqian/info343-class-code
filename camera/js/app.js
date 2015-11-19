4efb5ac8f12cfaa424bb9039797b87d77f27f9fd
Dave Stearns <dlsinfo@uw.edu>
2015-11-18 22:05:56 -0800

    finished video capture code

--- camera/js/app.js
+++ camera/js/app.js
@@ -10,19 +10,37 @@ document.addEventListener('DOMContentLoaded', function() {
     var video = document.querySelector('video');
     var canvas = document.querySelector('canvas');
     var ctx = canvas.getContext('2d');
-    var image = document.querySelector('img');
     var videoStream;
+    var mouseIsDown;
 
     function snapshot() {
         if (videoStream) {
             canvas.width = video.clientWidth;
             canvas.height = video.clientHeight;
             ctx.drawImage(video, 0, 0);
-            image.src = canvas.toDataURL('image/webp');
         }
     }
 
     video.addEventListener('click', snapshot, false);
+
+    canvas.addEventListener('mousedown', function(evt) {
+        ctx.beginPath();
+        ctx.moveTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
+        mouseIsDown = true;
+    });
+
+    canvas.addEventListener('mousemove', function(evt) {
+        if (mouseIsDown) {
+            ctx.lineTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
+            ctx.stroke();
+        }
+    });
+
+    canvas.addEventListener('mouseup', function(evt) {
+        ctx.lineTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
+        ctx.stroke();
+        mouseIsDown = false;
+    });
 
     navigator.getUserMedia({video: true}, function(stream) {
         videoStream = stream;
