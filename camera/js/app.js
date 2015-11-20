312daa135c20aa788cbb272a1919ea6c216bad9b
Yuting Qian <qianyu@uw.edu>
2015-11-19 17:06:28 -0800

    camera & notification

--- camera/js/app.js
+++ camera/js/app.js
@@ -2,6 +2,57 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    navigator.getUserMedia = navigator.getUserMedia
+        || navigator.webkitGetUserMedia
+        || navigator.mozGetUserMedia
+        || navigator.msGetUserMedia;
 
+    var video = document.querySelector('video');
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+    var videoStream;
+    var mouseIsDown;
+
+    navigator.getUserMedia({video: true}, function(stream) {
+        videoStream = stream;
+        video.src = window.URL.createObjectURL(stream);
+
+    }, function(err) {
+        console.error(err);
+    });
+
+    video.addEventListener('click', function() {
+        if (videoStream) {
+            canvas.width = video.clientWidth;
+            canvas.height = video.clientHeight;
+            ctx.drawImage(video, 0, 0);
+        }
+    });
+
+    canvas.addEventListener('mousedown', function(evt) {
+        var x = evt.clientX - canvas.offsetLeft;
+        var y = evt.clientY - canvas.offsetTop + window.scrollY;
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        mouseIsDown = true;
+    });
+
+    canvas.addEventListener('mousemove', function(evt) {
+        var x = evt.clientX - canvas.offsetLeft;
+        var y = evt.clientY - canvas.offsetTop + window.scrollY;
+
+        if (mouseIsDown) {
+            ctx.lineTo(x, y);
+            ctx.stroke();
+        }
+    });
+
+    canvas.addEventListener('mouseup', function(evt) {
+        mouseIsDown = false;
+    });
+
+    document.querySelector('#btnSnapshot').addEventListener('click', function() {
+        document.querySelector('img').src = canvas.toDataURL();
+    });
 });
 
--- notifications/js/app.js
+++ notifications/js/app.js
@@ -3,4 +3,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    function askPermision() {
+        Notification.requestPermission(function(result) {
+            if('granted' === result) {
+                showNotification('Thanks!', "You are an idiot!");
+            }
+        });
+    }
+
+
+
+    function showNotification(title, body) {
+        var note = new Notification(title, {body: body, icon: 'img/notification.png'});
+        window.setTimeout(note.close.bind(note), 3000);
+    }
+
+    var triggerBtn = document.getElementById('trigger');
+    triggerBtn.addEventListener('click', function() {
+       switch (Notification.permission) {
+           case 'granted':
+               showNotification('Hello', 'triggered at ' + new Date().toLocaleTimeString());
+               break;
+           case 'denied':
+               alter('Please enable notifications! Idiot...');
+               break;
+
+           default:
+               askPermision();
+       }
+    });
 });
